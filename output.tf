5570988fc124152fa3adc63fb0355ea6fc4f2ae0
표혜지 <pyohyeji@MacBook-Air.local>
2025-07-03 18:41:08 +0900

    retry

--- output.tf
+++ output.tf
@@ -1,5 +1,3 @@
 output "jenkins_public_ip" {
-    value = var.create_jenkins_instance ?
-        aws_instance.jenkins[0].public_ip :
-        data.aws_instance.jenkins_existing[0].public_ip
+  value = var.create_jenkins_instance ? aws_instance.jenkins[0].public_ip : data.aws_instance.jenkins_existing[0].public_ip
 }
